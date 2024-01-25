d1c8c843913a032aa9f757e2acb3e1aa2cfc4598
Daghlas Kaire <daghlaskaire58@gmail.com>
2024-01-25 12:08:09 +0300

    cards - extracting flutter widgets; card_bg class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'card_bg.dart';
 import 'quote.dart';
 
 void main() => runApp(const MaterialApp(
@@ -31,42 +32,6 @@ class _QuoteListState extends State<QuoteList> {
         body: Column(
           children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
         )
-    );
-  }
-}
-
-class QuoteCard extends StatelessWidget {
-
-  final Quote quote;
-  const QuoteCard({super.key, required this.quote});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              quote.text,
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.grey[600],
-              ),
-            ),
-            const SizedBox(height: 6.0),
-            Text(
-              quote.author,
-              style: TextStyle(
-                fontSize: 14.0,
-                color: Colors.grey[800],
-              ),
-            )
-          ],
-        ),
-      ),
     );
   }
 }
